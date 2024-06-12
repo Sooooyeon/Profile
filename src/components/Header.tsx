d08243da25614d9fe9868087297c05d49b5e1989
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-06-13 02:36:11 +0900

    header 분기, project style 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,37 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-type Props = {}
+function Header() {
+const {pathname} = useLocation();
+const navigate = useNavigate();
 
-function Header({}: Props) {
+
   return (
     <div className='w-screen bg-neutral-900 h-24 fixed top-0 left-1/2 transform -translate-x-1/2 px-4 border-b-2 border-gray-700'>
       <header className='max-w-6xl container mx-auto h-24  flex justify-between items-center gap-16'>
         
-          <a href='/'><img className='w-32' src={process.env.PUBLIC_URL + "profile-logo.png"} alt="Developer SooyeonJang" /></a>
-          <nav>
-              <ul className='flex space-x-8'>
-                <a href="#about" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>ABOUT ME</a>
-                <a href="#skills" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>SKILLS</a>
-                <a href="#project" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>PROJECTS</a>
-                <a href="#experience" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>EXPERIENCE</a>
-              </ul>
-          </nav>
+          
+          {
+            pathname !== '/project'? 
+            <>
+              <a href='/'><img className='w-32' src={process.env.PUBLIC_URL + "profile-logo.png"} alt="Developer SooyeonJang" /></a>
+              <nav>
+                <ul className='flex space-x-8'>
+                  <a href="#about" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>ABOUT ME</a>
+                  <a href="#skills" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>SKILLS</a>
+                  <a href="#project" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>PROJECTS</a>
+                  <a href="#experience" className='hover:text-amber-400 hover:scale-105 hover:transition-all'>EXPERIENCE</a>
+                </ul>
+              </nav>
+            </>
+          :
+            <>
+              <button onClick={()=> navigate(-1)} className='text-xl p-3 hover:text-amber-400 hover:scale-105 hover:transition-all'>← 이전페이지로</button>
+              <a href='/'><img className='w-32' src={process.env.PUBLIC_URL + "profile-logo.png"} alt="Developer SooyeonJang" /></a>
+            </>
+          }
+          
+          
       </header>
     </div>
   )
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,25 +5,41 @@ function Project() {
   return (
     <div className='max-w-5xl container mx-auto pt-28 tracking-wide' id='project'>
       <h3 className='py-10 text-3xl text-center'>PROJECTS</h3>
-        <div className='p-6 bg-gray-700 rounded-xl' >
+        <div className='p-10 bg-gray-700 rounded-xl flex flex-col items-center' >
         <div className='grid grid-cols-2 gap-3 justify-center text-center'>
-          <Link to='/project/moamoa' className='bg-white rounded-xl text-neutral-900 flex flex-col items-center'>
-            <h4>box1</h4>
-            <img src="" alt="" />
+          <Link to='/project/moamoa' className='bg-white rounded-xl text-neutral-900 flex flex-col items-center justify-between p-3'>
+            <img className=' mb-3' src={process.env.PUBLIC_URL + "moamoa.png"} alt="" />
+            <div>
+              <h4 className='text-xl'>MOAMOA<span className='text-sm'>(팀프로젝트)</span></h4>
+              <p>축제와 체험 정보를 공유하는 SNS 플랫폼</p>
+            </div>
           </Link>
-          <div className='bg-white rounded-xl text-neutral-900 flex flex-col items-center'>
-            <h4>box1</h4>
-            <img src="" alt="" />
+          <div className='bg-white rounded-xl text-neutral-900 flex flex-col items-center justify-between p-3'>
+            <img className=' mb-3' src={process.env.PUBLIC_URL + "obstacle-free.png"} alt="" />
+            <div>
+              <h4 className='text-xl'>Obstacle-Free</h4>
+              <p>관광지의 편의시설 정보를 확인할 수 있는 서비스</p>
+            </div>
           </div>
-          <div className='bg-white rounded-xl text-neutral-900 flex flex-col items-center'>
-            <h4>box1</h4>
-            <img src="" alt="" />
+          <div className='bg-white rounded-xl text-neutral-900 flex flex-col items-center justify-between p-3'>
+            <img className='mb-3' src={process.env.PUBLIC_URL + "cashbook.png"} alt="" />
+            <div>
+              <h4 className='text-xl'>CashBook</h4>
+              <p>가계부 응용프로그램</p>
+            </div>
           </div>
-          <div className='bg-white rounded-xl text-neutral-900 flex flex-col items-center'>
-            <h4>box1</h4>
-            <img src="" alt="" />
+          <div className='bg-white rounded-xl text-neutral-900 flex flex-col items-center justify-between p-3'>
+            <img className='mb-3' src={process.env.PUBLIC_URL + "pomodoro.png"} alt="" />
+            <div>
+              <h4 className='text-xl'>POMOODORO</h4>
+              <p>뽀모도로 타이머를 적용한 TodoList </p>
+            </div>
           </div>
         </div>
+        <div className='text-5xl leading-4 mt-4 text-gray-200'>.</div>
+        <div className='text-5xl leading-4 text-gray-300'>.</div>
+        <div className='text-5xl leading-4 text-gray-400'>.</div>
+        <Link to='/project' className='m-auto my-0 px-4 py-2 mt-4 bg-neutral-900 rounded-full hover:bg-amber-400 hover:text-neutral-900 hover:transition-all hover:duration-200 hover:font-bold'><span className='tracking-wider text-lg'>More</span></Link>
       </div>
     </div>
   )
